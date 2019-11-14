ae02dc29b5c99e76f3f8ea64e4788ad68d2e3dcf
Oğuzhan Göller <goller.oguzhan@gmail.com>
2019-11-14 22:56:35 +0300

    Initial commit

---
+++ lib/gravedigger.rb
@@ -0,0 +1,6 @@
+require "gravedigger/version"
+
+module Gravedigger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gravedigger/version.rb
@@ -0,0 +1,3 @@
+module Gravedigger
+  VERSION = "0.1.0"
+end
